9371772cd752181e1559aa37c1b152007b4f5c64
BeRobi <screengsm@gmail.com>
2023-02-23 18:05:21 +0100

    v2

--- Metodusok.java
+++ Metodusok.java
@@ -7,21 +7,32 @@ package metodusok;
 public class Metodusok {
 
     public static void main(String[] args) {
-
+        int osszeg = elso10szamOsszege();
+        String kimenet = "Az elsõ 10 szám összege: " + osszeg + "\n";
+        kiir(kimenet);
+        
+        int szam1 = 7, szam2 = 4;
+        kiir("%d + %d = %d\n".formatted(szam1, szam2, szam1+sza,2));
     }
 
-    public static void elso10SzamOsszege() {
+    private static int elso10SzamOsszege() {
 //összead
         int osszeg = 0;
         for (int i = 0; i < 10; i++) {
             osszeg += i;
         }
+        return osszeg;
 //kiír
-        System.out.println("Az elsõ 10 szám összege: " + osszeg);
+        //System.out.println("Az elsõ 10 szám összege: " + osszeg);
     }
 
-    public static void osszead(int a, int b) {
+    private static int osszead(int a, int b) {
+        return a + b;
 //összead és kiír
-        System.out.printf("%d + %d = %d\n", a, b, a+b);
+        //System.out.printf("%d + %d = %d\n", a, b, a + b);
+    }
+
+    private static void kiir(String szoveg) {
+        System.out.print(szoveg);
     }
 }
